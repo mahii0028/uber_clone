22756dff2e6b1551ce3b313a381001cf12bc5e2e
mahesh_kundagol_h <mahesh75481@gmail.com>
2024-12-13 15:04:49 +0530

    missed file pushed

--- Backend/services/user.service.js
+++ Backend/services/user.service.js
@@ -1,11 +1,6 @@
 const userModel = require("../models/user.model");
 
-module.exports.createUser = async ({
-  firstName,
-  lastName,
-  email,
-  password,
-}) => {
+module.exports.createUser = async ({ firstName, lastName, email, password }) => {
   if (!firstName || !email || !password) {
     throw new Error("All fields are required");
   }
