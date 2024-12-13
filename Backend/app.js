e542c5703d56ca030d42537c7c1ba464f8cda511
mahesh_kundagol_h <mahesh75481@gmail.com>
2024-12-13 15:02:55 +0530

    added profile and logout router

--- Backend/app.js
+++ Backend/app.js
@@ -3,6 +3,7 @@ dotenv.config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const cookieParser = require("cookie-parser");
 const connectToDataBase = require("./db/db");
 const userRoutes = require("./routes/user.routes");
 connectToDataBase();
@@ -10,6 +11,7 @@ connectToDataBase();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("Hello world");
